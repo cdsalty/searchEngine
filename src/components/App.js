d2af51f999ee6e66429f8a096f0a29e401050bfa
cdsalty <soltiscd@gmail.com>
2019-03-02 16:09:02 -0500

    added ASYNC AWAIT request

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import axios from 'axios';
-import SearchBar from './SearchBar';	// axios placed above because this is a user made component
+import axios from 'axios';	// axios placed above because this is a user made component
+import SearchBar from './SearchBar';	
 
 
 // STEP ONE
@@ -9,9 +9,9 @@ import SearchBar from './SearchBar';	// axios placed above because this is a use
         // Then went to our first created folder, index.js
 class App extends React.Component {
 // Creating a callback method on the App and then refer to it in your Seachbar JSX near bottomn
-	onSearchSubmit(term) {
+	async onSearchSubmit(term) {
 		// console.log(term);
-		axios.get('https://api.unsplash.com/search/photos', {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			// params: specifies the different query string parameters we want to add to this request
 			// see https://unsplash.com/documentation#search-photos for full list of all params available
 			params: { query : term },	// calling query from documentation above and passing (terms)  
@@ -19,8 +19,10 @@ class App extends React.Component {
 				Authorization: 'Client-ID 05e23c603b63c6549e8bb4d1f64dad26360985c386f9a77b390f040a7ffbe3ac'
 			}
 		});
+
+		console.log(response.data.results);
 	}
-		
+// To return our data from the api, we also could have used a Promise, then dealt with .then()
 
 render(){        
 	return ( 
