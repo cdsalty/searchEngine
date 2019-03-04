39f917cceccafb6a61a464ff7222e72d0e673bad
cdsalty <soltiscd@gmail.com>
2019-03-04 15:58:22 -0500

    rendered results, need to fix small error

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);  // Shows our full array of 10 picture objects
-    return <div>ImageList</div>
+    // console.log(props.images);  // Shows our full array of 10 picture objects
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />
+    });
+    return <div>{images}</div>;
 };
 
 export default ImageList;
