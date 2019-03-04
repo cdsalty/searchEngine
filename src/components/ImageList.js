07939ef298b160a641fd0568207960c200600ba2
cdsalty <soltiscd@gmail.com>
2019-03-04 15:59:01 -0500

    one more updated file

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageList = (props) => {
     // console.log(props.images);  // Shows our full array of 10 picture objects
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+        return <img src={image.urls.regular} />;
     });
     return <div>{images}</div>;
 };
